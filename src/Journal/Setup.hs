355ed73cc0cb95228dbbf58bafa57eeb13948735
Julien Blanchard <julien@sideburns.eu>
2018-01-19 17:49:40 +0100

    Copy assets in _site

--- src/Journal/Setup.hs
+++ src/Journal/Setup.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Journal.Setup where
 
-import           System.Directory (createDirectoryIfMissing)
+import           System.Directory (copyFile, createDirectoryIfMissing)
 
 
 buildSkeleton :: IO()
 buildSkeleton = do
-  let dirs = ["_layout", "_posts", "_site"]
-  mapM_ (\d -> createDirectoryIfMissing False d) dirs
+  putStrLn "Building skeleton..."
+  let dirs = ["_layout", "_posts", "_site/assets/css"]
+  mapM_ (\d -> createDirectoryIfMissing True d) dirs
+  copyFile "assets/style.css" "_site/assets/css/style.css"
