a448e360809d73112e1ef64228cac7eadc1dd5aa
Julien Blanchard <julien@sideburns.eu>
2018-01-19 17:49:05 +0100

    Cleanup app/Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Data.String.Conversions       (cs)
-import           Data.Text                     (Text (..))
-import           System.Directory              (listDirectory)
-import           System.FilePath.Posix
-import           Text.Blaze.Html.Renderer.Text (renderHtml)
-import           Text.Markdown                 (def, markdown, msXssProtect)
-
-import           Journal.Reader                (readPosts)
-import           Journal.Setup                 (buildSkeleton)
-import           Journal.Writer                (convertMdToHtml)
+import           Journal.Reader (readPosts)
+import           Journal.Setup  (buildSkeleton)
+import           Journal.Writer (convertMdToHtml)
 
 main :: IO ()
 main = do
