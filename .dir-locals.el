b3f194cc15e91244e1c89bad9f2a45b902c11738
Julien Blanchard <julien@sideburns.eu>
2018-01-31 12:20:02 +0100

    Ignore posts, set intero targets

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "journal:lib" "journal:exe:journal" "journal:test:journal-test")))
