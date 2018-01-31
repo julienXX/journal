192afae1693cc47f26700e07b2bfb212938a0531
Julien Blanchard <julien@sideburns.eu>
2018-01-31 12:17:50 +0100

    Pass date to templates

--- src/Journal/Writer.hs
+++ src/Journal/Writer.hs
@@ -3,13 +3,14 @@ module Journal.Writer where
 
 import           Data.String.Conversions       (cs)
 import           Data.Text                     as T
-import           Data.Text.Lazy                as LT
+import           Data.Text.Lazy                as LT hiding (pack, unpack)
 import           System.FilePath.Posix         (dropExtension)
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
 import           Text.EDE                      (eitherParseFile, eitherRender,
                                                 fromPairs, (.=))
 import           Text.Markdown                 (def, markdown, msXssProtect)
 
+import           Journal.Post
 import           Journal.PostParser
 
 convertMdToHtml :: FilePath -> IO()
@@ -18,28 +19,22 @@ convertMdToHtml file = do
   let post = parsePost $ cs source
   let postTitle = getTitle post
   let postBody = getBody post
+  let postDate = getDate post
   let renderedMarkdown = renderHtml $ markdown def { msXssProtect = True } $ cs postBody
   let dest = destPath file
-  generatedHtml <- mkHtml postTitle renderedMarkdown
+  generatedHtml <- mkHtml postTitle postDate renderedMarkdown
   writeFile dest (cs generatedHtml)
 
 destPath :: FilePath -> FilePath
 destPath f = "_site/" ++ dropExtension f ++ ".html"
 
-mkHtml :: T.Text -> LT.Text -> IO LT.Text
-mkHtml pTitle pBody = do
+mkHtml :: T.Text -> T.Text -> LT.Text -> IO LT.Text
+mkHtml pTitle pDate pBody = do
     r <- eitherParseFile "_layout/default.ede"
     either error return $ r >>= (`eitherRender` values)
   where
     values = fromPairs
         [ "title" .= pTitle
+        , "date"  .= pDate
         , "body"  .= pBody
         ]
-
-getTitle :: Either T.Text Post -> T.Text
-getTitle (Left _)  = ""
-getTitle (Right p) = cs $ title p
-
-getBody :: Either T.Text Post -> T.Text
-getBody (Left _)  = ""
-getBody (Right p) = cs $ body p
